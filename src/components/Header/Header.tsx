f48ccc3babfecf3dc7194aa48e1dfaf6fe186f1e
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-05-01 16:54:21 -0300

    initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+
+    return (
+        <div>
+            <nav>
+                <p>About</p>
+                <p>Experience</p>
+                <p>Skills</p>
+                <p>Projects</p>
+            </nav>
+            
+        </div>
+    )
+}
