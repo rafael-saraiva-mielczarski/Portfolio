ee181496aad7d9bda66fa0a24e50c339feacbc30
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-05-02 11:24:35 -0300

    fix build apos issue

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -10,8 +10,8 @@ export default function About() {
             <Container>
                 <div className={styles.about}>
                     <div className={styles.text}>
-                        <h1>Hello! I'm <br /><span>Rafael Saraiva</span><br/>Software Engineer</h1>
-                        <p>An early professional aspiring to impact positively the world around me trough code.  Get to know me better in this personal website, find what I've been studying and working on, my skills and experiences and contacts. Feel free to explore!</p>
+                        <h1>Hello! I&apos;m <br /><span>Rafael Saraiva</span><br/>Software Engineer</h1>
+                        <p>An early professional aspiring to impact positively the world around me trough code.  Get to know me better in this personal website, find what I&apos;ve been studying and working on, my skills and experiences and contacts. Feel free to explore!</p>
                     </div>
                     <Image src={profile} alt="profile art made with Leonardo AI" className={styles.profile}/>
                     <div className={styles.blur}></div>
